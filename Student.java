57107e0437b785ae71b9badb24a390d8cf4fd5db
Filip Kuzmanov <kuzmanov.filip2210@gmail.com>
2020-04-10 21:27:36 +0200

    labPoints added

---
+++ Student.java
@@ -0,0 +1,19 @@
+class Student {
+	String index;
+	String firstName;
+	String lastName;
+	
+	List<Integer> labPoints = new List<Integer>();
+
+	//TODO constructor
+
+	//TODO seters & getters
+
+	public double getAverage() {
+		//TODO
+	}
+
+	public boolean hasSignature() {
+		//TODO
+	}
+}
