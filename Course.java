f73791c7d2eaed61ad8b595e9787de632976e171
Filip Kuzmanov <kuzmanov.filip2210@gmail.com>
2020-04-10 21:54:10 +0200

    add course class and update students.csv

---
+++ Course.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Course {
+	
+	List<Student> studentList = new ArrayList<Student>();
+	
+	public void addStudent() {
+		// TO DO
+	}
+	
+	public void removeStudent() {
+		// TO DO
+	}
+
+}
